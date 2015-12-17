b4d8d7801883e5e5c7bf293f8a5b17f8954db94a
slientcloud <rjmuqiang@gmail.com>
2015-12-17 23:22:00 +0800

    add coverage

--- karma.conf.js
+++ karma.conf.js
@@ -9,19 +9,6 @@ var webpackConfig = getWebpackCommonConfig({
 
 webpackConfig.devtool = 'inline-source-map';
 
-webpackConfig.plugins.push(
-  new webpack.ProgressPlugin(function(percentage, msg) {
-    var stream = process.stderr;
-    if (stream.isTTY && percentage < 0.71) {
-      stream.cursorTo(0);
-      stream.write(msg);
-      stream.clearLine(1);
-    } else if (percentage === 1) {
-      console.log('\nwebpack: bundle build is now finished.');
-    }
-  })
-);
-
 // CommonsChunkPlugin 出来的common.js 会导致报错
 // https://github.com/webpack/karma-webpack/issues/24
 webpackConfig.plugins = [
@@ -41,18 +28,30 @@ webpackConfig.plugins = [
 // https://github.com/webpack/webpack/issues/304
 //webpackConfig.module.noParse = [/\/sinon.js/];
 
-// TODO 测试覆盖率
-//webpackConfig.module.loaders.push(
-//  { test: /\.jsx?$/, include:/src/, loader: 'isparta' }
-//)
+// 测试覆盖率
+//var preLoaders = [
+//  {
+//    test: /\.jsx?$/,
+//    //include: /src/, //TODO  优化: 拿到源码路径比用 exclude 好
+//    exclude: /(__tests__|node_modules|bower_components)/,
+//    loader: 'isparta'
+//  }
+//];
+//
+//if (webpackConfig.module.preLoaders) {
+//  webpackConfig.module.preLoaders.concat(preLoaders);
+//} else {
+//  webpackConfig.module.preLoaders = preLoaders;
+//}
 
-var base_test_dir = 'test';
+var base_test_dir = '__test__';
 var testArgs = process.argv.slice(4);
 var testParam = testArgs.indexOf('--test-dir');
 if (testParam > -1) {
   base_test_dir = testArgs[testParam + 1];
 }
 
+// files to test
 var files_to_test = path.resolve(cwd, './'+ base_test_dir +'/**/*-test.js');
 var preprocessor = {};
 preprocessor[files_to_test] = ['webpack', 'sourcemap'];
@@ -88,49 +87,21 @@ module.exports = function (config) {
 
     preprocessors: preprocessor,
 
+    // 不能在 terminal 输出 coverage 的同时,生成其他格式的 reporters
     coverageReporter: {
       dir : path.join(cwd, 'coverage'),
       reporters: [
         { type: 'html', subdir: 'report-html' },
         { type: 'lcov', subdir: 'report-lcov' },
-        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' }
-      ],
-      instrumenters: {
-        isparta : require('isparta')
-      },
-      instrumenter: {
-        '/Users/silentcloud/Desktop/atool-test-testing/src/**/*.js': 'isparta'
-      },
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir:'.', file: 'text-summary.txt' },
+        { type: 'json-summary', subdir:'.', file: 'json-summary.json'}
+      ]
     },
 
     webpack: webpackConfig,
-    /* webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        //loaders: [
-        //  { test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-        //]
-        isparta: {
-          embedSource: true,
-          noAutoWrap: true,
-          babel: {
-            presets: ['es2015', 'react']
-          }
-        },
-        preLoaders: [
-          {
-            test: /\.js$/,
-            exclude: [/node_modules/, path.join(cwd, 'src')],
-            loader: 'babel-loader'
-          },
-          {
-            test: /\.js$/,
-            include: path.join(cwd, 'src'),
-            loader: 'isparta'
-          }
-        ]
-      }
-    },*/
 
     webpackServer: {
       noInfo: true
