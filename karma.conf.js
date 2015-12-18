fbe84c2aeff2f20f916ea762e39842d632915b91
slientcloud <rjmuqiang@gmail.com>
2015-12-18 14:56:37 +0800

    add coverage & self loader

--- karma.conf.js
+++ karma.conf.js
@@ -1,48 +1,8 @@
 var path = require('path');
 var cwd = process.cwd();
-var getWebpackCommonConfig = require('atool-build/lib/getWebpackCommonConfig');
 var webpack = require('webpack');
-
-var webpackConfig = getWebpackCommonConfig({
-  cwd: cwd
-});
-
-webpackConfig.devtool = 'inline-source-map';
-
-// CommonsChunkPlugin 出来的common.js 会导致报错
-// https://github.com/webpack/karma-webpack/issues/24
-webpackConfig.plugins = [
-  new webpack.ProgressPlugin(function(percentage, msg) {
-    var stream = process.stderr;
-    if (stream.isTTY && percentage < 0.71) {
-      stream.cursorTo(0);
-      stream.write(msg);
-      stream.clearLine(1);
-    } else if (percentage === 1) {
-      console.log('\nwebpack: bundle build is now finished.');
-    }
-  }),
-  //new webpack.NormalModuleReplacementPlugin(/^sinon$/, './setup/sinon-1.17.2.js'),
-];
-
-// https://github.com/webpack/webpack/issues/304
-//webpackConfig.module.noParse = [/\/sinon.js/];
-
-// 测试覆盖率
-//var preLoaders = [
-//  {
-//    test: /\.jsx?$/,
-//    //include: /src/, //TODO  优化: 拿到源码路径比用 exclude 好
-//    exclude: /(__tests__|node_modules|bower_components)/,
-//    loader: 'isparta'
-//  }
-//];
-//
-//if (webpackConfig.module.preLoaders) {
-//  webpackConfig.module.preLoaders.concat(preLoaders);
-//} else {
-//  webpackConfig.module.preLoaders = preLoaders;
-//}
+var getTestWebpackConfig = require('./lib/getTestWebpackConfig');
+var webpackConfig = getTestWebpackConfig();
 
 var base_test_dir = '__test__';
 var testArgs = process.argv.slice(4);
@@ -50,7 +10,6 @@ var testParam = testArgs.indexOf('--test-dir');
 if (testParam > -1) {
   base_test_dir = testArgs[testParam + 1];
 }
-
 // files to test
 var files_to_test = path.resolve(cwd, './'+ base_test_dir +'/**/*-test.js');
 var preprocessor = {};
@@ -87,7 +46,7 @@ module.exports = function (config) {
 
     preprocessors: preprocessor,
 
-    // 不能在 terminal 输出 coverage 的同时,生成其他格式的 reporters
+    // karma-coverage 不能在 terminal 输出 coverage 的同时,生成其他格式的 reporters
     coverageReporter: {
       dir : path.join(cwd, 'coverage'),
       reporters: [
