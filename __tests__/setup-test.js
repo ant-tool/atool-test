1ed148652caf300eed29d38a6029ace8cdc2953e
slientcloud <rjmuqiang@gmail.com>
2016-01-07 14:41:51 +0800

    upadte tests

--- __tests__/setup-test.js
+++ __tests__/setup-test.js
@@ -2,6 +2,5 @@ describe('global check', function() {
   it('should have expect && TestUtils', function () {
     expect(expect).not.to.be.null;
     expect(sinon).not.to.be.null;
-    expect(TestUtils).not.to.be.null;
   });
 });
